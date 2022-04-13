75660a678ffb29cec61766f2c96428b4ad19e268
Najib <brimahayo@gmail.com>
2022-04-13 16:34:58 -0700

    form validation on

---
+++ app.js
@@ -0,0 +1,31 @@
+// variables
+const submitForm = document.querySelector('.form');
+const email = document.querySelector('#email');
+const firstName = document.querySelector("#firstname");
+const lastName = document.querySelector("#lastname");
+const password = document.querySelector("#password");
+
+
+
+
+
+
+
+eventListeners();
+
+function eventListeners() {
+    submitForm.addEventListener('submit', validateForm);
+
+}
+ 
+
+
+
+
+//functions
+function validateForm(e) {
+    e.preventDefault();
+    const 
+    if ()
+    
+}
