1a08b7b9cc6bdb1774bf01a7bb96930519a963e9
Najib <brimahayo@gmail.com>
2022-04-14 18:11:47 -0700

    the form now displays errors succcesfully, but still stuck on how to get rid of errors when the user inputs details correctly. And also to prevent the error from duplicating upon multiple clicks

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // variables
-const submitForm = document.querySelector('.form');
+submitForm = document.querySelector('.form');
 const email = document.querySelector('#email');
 const firstName = document.querySelector("#firstname");
 const lastName = document.querySelector("#lastname");
@@ -25,7 +25,31 @@ function eventListeners() {
 //functions
 function validateForm(e) {
     e.preventDefault();
-    const 
-    if ()
-    
+    validate(email);
+    validate(firstName);
+    validate(lastName);
+    validate(password);
+}
+
+function validate(field) {
+    if (field.value === '') {
+    const error = document.createElement('p');
+        error.textContent = `${field.placeholder} cannot be empty`;
+        error.className = 'error';
+        field.parentElement.appendChild(error);
+
+    const errorIcon = document.createElement('img');
+        errorIcon.src = 'images/icon-error.svg';
+        errorIcon.className = 'error-icon';
+        field.parentElement.appendChild(errorIcon);
+        
+        field.style.border = "2px solid hsl(0, 100%, 74%)";
+    }
+
+    if (field.parentElement.classList.contains()) {
+
+    }
+
+
+
 }
