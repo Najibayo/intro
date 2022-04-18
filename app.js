f58b2ed768fb6ba190fcafa60fca96d1dec8dcb2
Najib <brimahayo@gmail.com>
2022-04-18 15:04:02 -0700

    done

--- app.js
+++ app.js
@@ -24,30 +24,35 @@ function eventListeners() {
 
 //functions
 function validateForm(e) {
-    e.preventDefault();
-    validate(email);
-    validate(firstName);
-    validate(lastName);
-    validate(password);
+  e.preventDefault();
+  validate(email);
+  validate(firstName);
+  validate(lastName);
+  validate(password);
 }
 
 function validate(field) {
-    if (field.value === '') {
-    const error = document.createElement('p');
-        error.textContent = `${field.placeholder} cannot be empty`;
-        error.className = 'error';
-        field.parentElement.appendChild(error);
+  let error = field.parentElement.querySelector("p.error");
+  let errorIcon = field.parentElement.querySelector("img.error-icon");
 
-    const errorIcon = document.createElement('img');
-        errorIcon.src = 'images/icon-error.svg';
-        errorIcon.className = 'error-icon';
-        field.parentElement.appendChild(errorIcon);
+  if (!error && field.value === "") {
+    error = document.createElement("p");
+    errorIcon = document.createElement("img");
 
-        field.style.border = "2px solid hsl(0, 100%, 74%)";
-    }
+    error.textContent = `${field.placeholder} cannot be empty`;
+    error.className = "error";
+    field.parentElement.appendChild(error);
 
-    
+    errorIcon.src = "images/icon-error.svg";
+    errorIcon.className = "error-icon";
+    field.parentElement.appendChild(errorIcon);
 
-
-
+    field.style.border = "2px solid hsl(0, 100%, 74%)";
+  } else if (error && field.value !== "") {
+    field.style.border = "solid 1px hsl(246, 25%, 77%";
+    field.parentElement.removeChild(error);
+    field.parentElement.removeChild(errorIcon);
+  }
 }
+ 
+
